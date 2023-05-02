d184aec7f474c940efa023a8d31a8b1f39415ceb
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-03 00:24:27 +0100

    add task 0

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,13 +12,19 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+@app.errorhandler(401)
+def not_found(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
