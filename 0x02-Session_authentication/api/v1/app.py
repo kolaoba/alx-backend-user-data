168fc0aaa53b1f02eacc6ff4d17075ffd062f59d
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-06 16:14:32 +0100

    add task 4

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,8 +55,10 @@ def before_request():
         return
     if auth.require_auth(request.path, ['/api/v1/status/',
                                         '/api/v1/unauthorized/',
-                                        '/api/v1/forbidden/']):
-        if auth.authorization_header(request) is None:
+                                        '/api/v1/forbidden/',
+                                        '/api/v1/auth_session/login/']):
+        if auth.authorization_header(
+                request) is None or auth.session_cookie(request) is None:
             abort(401)
         elif auth.current_user(request) is None:
             abort(403)
