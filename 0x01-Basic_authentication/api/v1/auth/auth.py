50b8097191db1b3c6c1c75649bfc9b5ea3e0bffb
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-06 13:51:46 +0100

    add lint

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,15 +7,15 @@ from typing import List, TypeVar
 
 class Auth:
     """Defines Auth Class"""
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """rdefines require auth function"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """defines authorization header"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """returns current user"""
         return None
