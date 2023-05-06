c18c541efad637969939bafc6ccd88c70ee19e19
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-06 15:42:19 +0100

    move files and add task 0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Define Auth Class"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Defines Auth Class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """defines require auth function"""
+
+        if path is None or excluded_paths is None:
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """defines authorization header"""
+
+        # if request is None or 'Authotization' not in request.headers:
+        #     return None
+        return request.headers.get('Authorization') if request else None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns current user"""
+        return None
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+BasicAuth that inherits from Auth
+"""
+
+from api.v1.auth.auth import Auth
+import base64
+import binascii
+from models.user import User
+from typing import Tuple, TypeVar
+
+
+class BasicAuth(Auth):
+    """class inherits from auth"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """returns Base64 part of the Authorization header"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns decoded value of Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except binascii.Error:
+            return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """returns user email and password from Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        usr_creds = decoded_base64_authorization_header.split(':')
+        return usr_creds[0], usr_creds[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on email and password"""
+        if not isinstance(user_email, str):
+            return None
+        if not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        if len(users) == 0:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """overloads Auth and retrieves User instance for a request"""
+        auth_header = self.authorization_header(request)
+        base64_auth_token = self.extract_base64_authorization_header(
+            auth_header)
+        auth_token = self.decode_base64_authorization_header(base64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
