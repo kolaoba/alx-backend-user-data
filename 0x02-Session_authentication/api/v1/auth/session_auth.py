b1fa7c05ad7ff04cc5fd332e35647f64cc41a1e4
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-06 16:20:15 +0100

    add task 5

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ SessionAuth that inherits from Auth
 """
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models import User
 
 
 class SessionAuth(Auth):
@@ -29,3 +30,8 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """returns user instance based on cookie value"""
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
