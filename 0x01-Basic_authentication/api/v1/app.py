dd50b863681f948a8d7a59b3777cbfcdad7b144e
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-06 14:13:25 +0100

    add task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -13,8 +14,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+auth = None
+auth_type = os.getenv("AUTH_TYPE")
 
-
+if auth_type == 'auth':
+    auth = Auth()
+    
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -34,6 +39,19 @@ def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+@app.before_request
+def before_request():
+    """filter each request"""
+    if auth is None:
+        return
+    if auth.require_auth(request.path, ['/api/v1/status/',
+                                        '/api/v1/unauthorized/',
+                                        '/api/v1/forbidden/']):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        elif auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,7 +19,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """defines authorization header"""
-        return None
+        
+        # if request is None or 'Authotization' not in request.headers:
+        #     return None
+        return request.headers.get('Authorization') if request else None
+        
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns current user"""
