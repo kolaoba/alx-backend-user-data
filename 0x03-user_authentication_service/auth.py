0e71b38c8ea81ffec84abc2aa4d72bcf60c80b65
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-14 18:46:21 +0100

    add task 4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defines Auth"""
+
+from typing import ByteString
+import bcrypt
+
+def _hash_password(password: str) -> ByteString:
+    """hashes password, returns bytes"""
+    return bcrypt.hashpw(password.encode('utf-8'), salt=bcrypt.gensalt())
