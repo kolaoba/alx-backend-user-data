96c2eb3583ce737c8e7abdb97196429e42d79428
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-14 17:35:02 +0100

    add type annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,7 +44,7 @@ class DB:
             user = None
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """Find user by arbitraty input"""
         if not kwargs:
             raise InvalidRequestError
